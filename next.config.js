e2648d53c5c4e70065c604a8cf46fb19c6cbad14
MMMuteti <mmmuteti@gmail.com>
2024-12-03 09:25:04 +0300

    Resolved next.config.js conflict

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 pages/api/fetchSLAs.js
 
 export default async function handler(req, res) {
-<<<<<<< HEAD
   const response = await fetch("http://localhost:5500/api/slas");
   const data = await response.json();
   res.status(200).json(data);
@@ -10,9 +9,3 @@ export default async function handler(req, res) {
 node_modules/
 .next/
 .env
-=======
-  const response = await fetch("http://localhost:5000/api/slas");
-  const data = await response.json();
-  res.status(200).json(data);
-}
->>>>>>> master
